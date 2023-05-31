ebd6a33e4efc9f7cbcb24fc0268df17c6aaabe3a
Alexander Baldauf <baldauf@gruener-campus-malchow.de>
2023-05-31 10:56:57 +0200

    obstacle

--- winding_machine/code fragments/test_servo_obstacle.py
+++ winding_machine/code fragments/test_servo_obstacle.py
@@ -6,9 +6,9 @@ import time
 servoPIN = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
-servo = GPIO.PWM(servoPIN, 50) # GPIO 18 als PWM mit 50Hz
-dutyCycle = 7.2
-steps = 0.05
+global servo = GPIO.PWM(servoPIN, 50) # GPIO 18 als PWM mit 50Hz
+global dutyCycle = 7.2
+global steps = 0.05
 
 ObstaclePin = 27
 
