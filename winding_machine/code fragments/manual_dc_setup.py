82010ac1cc8c38e91c09a905ff19682009a22d27
Alexander Baldauf <baldauf@gruener-campus-malchow.de>
2023-05-31 11:33:59 +0200

    manual dc setup

---
+++ winding_machine/code fragments/manual_dc_setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import RPi.GPIO as GPIO
+import time
+
+servoPIN = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+servo = GPIO.PWM(servoPIN, 50) # GPIO 18 als PWM mit 50Hz
+dutyCycle = 7.2
+steps = 0.05
+servo.start(0) # Initialisierung
+
+zahl = float(input("Bitte neuen Tastgrad eingeben: "))
+print = "Der neue Tastgrad ist" + str(zahl)
